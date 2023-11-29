ea270ad014b89eb7bbc4118aa8c00dedbe8f87a8
Yevhen Petryk <yevhen.petryk@sohohouse.com>
2023-11-29 12:19:49 +0200

    Update tests for EC2 task

--- src/tests/ec2/ec2.spec.ts
+++ src/tests/ec2/ec2.spec.ts
@@ -1,13 +1,15 @@
 import { expect } from 'chai';
 import axios from 'axios';
-import { EC2Client, DescribeInstancesCommand, DescribeSecurityGroupsCommand } from '@aws-sdk/client-ec2';
+import {
+  EC2Client,
+  DescribeInstancesCommand,
+  DescribeSecurityGroupsCommand,
+  DescribeVolumesCommand,
+} from '@aws-sdk/client-ec2';
 import { BaseConfig } from '../../BaseConfig';
 
 describe('EC2', () => {
   const { accessKeyId, secretAccessKey, region } = BaseConfig;
-
-  let publicIpv4Address: string = null;
-  let privateIpv4Address: string = null;
 
   let ec2: EC2Client = null;
   let deployedInstances: any[] = null;
@@ -49,40 +51,80 @@ describe('EC2', () => {
     }, []);
   });
 
-  it('Two application instances should be deployed (public and private)', () => {
-    expect(deployedInstances).to.have.length(2);
-
-    // Check public instance
-    const publicInstance = deployedInstances.find((instance) => instance.type === 'public');
-
-    expect(publicInstance.type).to.equal('public');
-    expect(publicInstance.instanceType).to.equal('t2.micro');
-    expect(publicInstance.tags.find((tag) => tag.Key === 'Name').Value).to.equal('cloudxinfo/PublicInstance/Instance');
-    expect(publicInstance.tags.find((tag) => tag.Key === 'cloudx').Value).to.equal('qa');
-    expect(publicInstance.os.PlatformDetails).to.equal('Linux/UNIX');
-    expect(publicInstance.os.PublicIpAddress).to.exist.and.not.be.empty;
-
-    // Check private instance
-    const privateInstance = deployedInstances.find((instance) => instance.type === 'private');
-
-    expect(privateInstance.type).to.equal('private');
-    expect(privateInstance.instanceType).to.equal('t2.micro');
-    expect(privateInstance.tags.find((tag) => tag.Key === 'Name').Value).to.equal(
-      'cloudxinfo/PrivateInstance/Instance',
-    );
-    expect(privateInstance.tags.find((tag) => tag.Key === 'cloudx').Value).to.equal('qa');
-    expect(privateInstance.os.PlatformDetails).to.equal('Linux/UNIX');
-    expect(privateInstance.os?.PublicIpAddress).to.be.undefined;
-    expect(privateInstance.os.PrivateIpAddress).to.exist.and.not.be.empty;
-
-    publicIpv4Address = publicInstance.os.PublicIpAddress;
-    privateIpv4Address = publicInstance.os.PrivateIpAddress;
+  it('Should create two application instances', () => {
+    expect(deployedInstances, 'The number of deployed instances is not correct').to.have.length(2);
   });
 
-  it(`Should have security groups' configuration`, async () => {
-    // Get the security group IDs associated with the public instance
+  it('Should return public instance configuration', () => {
     const publicInstance = deployedInstances.find((instance) => instance.type === 'public');
-    const publicSecurityGroupIds = publicInstance.os.SecurityGroups.map((group) => group.GroupId);
+
+    expect(publicInstance.type, 'Type of instance is not correct').to.equal('public');
+    expect(publicInstance.instanceType, 'Instance type is not correct').to.equal('t2.micro');
+    expect(publicInstance.tags.find((tag) => tag.Key === 'Name').Value, `Tag 'Name' is not correct`).to.equal(
+      'cloudxinfo/PublicInstance/Instance',
+    );
+    expect(publicInstance.tags.find((tag) => tag.Key === 'cloudx').Value, `Tag 'cloudx' is not correct`).to.equal('qa');
+    expect(publicInstance.os.PlatformDetails, `'os.PlatformDetails' is not correct`).to.equal('Linux/UNIX');
+    expect(publicInstance.os.PublicIpAddress, `'os.PublicIpAddress' is not correct`).to.exist.and.not.be.empty;
+  });
+
+  it('Should return private instance configuration', () => {
+    const privateInstance = deployedInstances.find((instance) => instance.type === 'private');
+
+    expect(privateInstance.type, 'Type of instance is not correct').to.equal('private');
+    expect(privateInstance.instanceType, 'Instance type is not correct').to.equal('t2.micro');
+    expect(privateInstance.tags.find((tag) => tag.Key === 'Name').Value, `Tag 'Name' is not correct`).to.equal(
+      'cloudxinfo/PrivateInstance/Instance',
+    );
+    expect(privateInstance.tags.find((tag) => tag.Key === 'cloudx').Value, `Tag 'cloudx' is not correct`).to.equal(
+      'qa',
+    );
+    expect(privateInstance.os.PlatformDetails, `'os.PlatformDetails' is not correct`).to.equal('Linux/UNIX');
+    expect(privateInstance.os?.PublicIpAddress, `'os.PublicIpAddress' is not correct`).to.be.undefined;
+    expect(privateInstance.os.PrivateIpAddress, `'os.PrivateIpAddress' is not correct`).to.exist.and.not.be.empty;
+  });
+
+  it('Should return public instances volumes', async () => {
+    const instanceId = deployedInstances.find((instance) => instance.type === 'public').id;
+
+    const params = {
+      Filters: [
+        {
+          Name: 'attachment.instance-id',
+          Values: [instanceId],
+        },
+      ],
+    };
+
+    const data = await ec2.send(new DescribeVolumesCommand(params));
+
+    expect(data.Volumes[0].Size, `'Volumes.Size' is not correct`).to.equal(8);
+    expect(data.Volumes[0].VolumeType, `'Volumes.VolumeType' is not correct`).to.equal('gp2');
+  });
+
+  it('Should return private instances volumes', async () => {
+    const instanceId = deployedInstances.find((instance) => instance.type === 'private').id;
+
+    const params = {
+      Filters: [
+        {
+          Name: 'attachment.instance-id',
+          Values: [instanceId],
+        },
+      ],
+    };
+
+    const data = await ec2.send(new DescribeVolumesCommand(params));
+
+    expect(data.Volumes[0].Size, `'Volumes.Size' is not correct`).to.equal(8);
+    expect(data.Volumes[0].VolumeType, `'Volumes.VolumeType' is not correct`).to.equal('gp2');
+  });
+
+  it(`Should return security groups configuration for the instances`, async () => {
+    // Get the security group IDs associated with the public instance
+    const publicSecurityGroupIds = deployedInstances
+      .find((instance) => instance.type === 'public')
+      .os.SecurityGroups.map((group) => group.GroupId);
 
     // Describe security groups for the public instance
     const publicSecurityGroups = await ec2.send(
@@ -91,10 +133,12 @@ describe('EC2', () => {
       }),
     );
 
-    // Assertions for public instance security group
     const publicSecurityGroup = publicSecurityGroups.SecurityGroups[0];
 
-    expect(publicSecurityGroup.IpPermissions).to.deep.include({
+    expect(
+      publicSecurityGroup.IpPermissions,
+      `Public Security Group IpPermissions for port 80 are not correct`,
+    ).to.deep.include({
       FromPort: 80,
       IpProtocol: 'tcp',
       IpRanges: [{ CidrIp: '0.0.0.0/0', Description: 'HTTP from Internet' }],
@@ -104,7 +148,10 @@ describe('EC2', () => {
       UserIdGroupPairs: [],
     });
 
-    expect(publicSecurityGroup.IpPermissions).to.deep.include({
+    expect(
+      publicSecurityGroup.IpPermissions,
+      `Public Security Group IpPermissions for port 22 are not correct`,
+    ).to.deep.include({
       FromPort: 22,
       IpProtocol: 'tcp',
       IpRanges: [{ CidrIp: '0.0.0.0/0', Description: 'SSH from Internet' }],
@@ -118,8 +165,9 @@ describe('EC2', () => {
     const ownerId: string = publicSecurityGroup.OwnerId;
 
     // Get the security group IDs associated with the private instance
-    const privateInstance = deployedInstances.find((instance) => instance.type === 'private');
-    const privateSecurityGroupIds = privateInstance.os.SecurityGroups.map((group) => group.GroupId);
+    const privateSecurityGroupIds = deployedInstances
+      .find((instance) => instance.type === 'private')
+      .os.SecurityGroups.map((group) => group.GroupId);
 
     // Describe security groups for the private instance
     const privateSecurityGroups = await ec2.send(
@@ -128,10 +176,12 @@ describe('EC2', () => {
       }),
     );
 
-    // Assertions for private instance security group
     const privateSecurityGroup = privateSecurityGroups.SecurityGroups[0];
 
-    expect(privateSecurityGroup.IpPermissions).to.deep.include({
+    expect(
+      privateSecurityGroup.IpPermissions,
+      `Private Security Group IpPermissions for port 80 are not correct`,
+    ).to.deep.include({
       FromPort: 80,
       IpProtocol: 'tcp',
       IpRanges: [],
@@ -141,7 +191,10 @@ describe('EC2', () => {
       UserIdGroupPairs: [{ Description: 'HTTP from Internet', GroupId: groupId, UserId: ownerId }],
     });
 
-    expect(privateSecurityGroup.IpPermissions).to.deep.include({
+    expect(
+      privateSecurityGroup.IpPermissions,
+      `Private Security Group IpPermissions for port 22 are not correct`,
+    ).to.deep.include({
       FromPort: 22,
       IpProtocol: 'tcp',
       IpRanges: [],
@@ -153,12 +206,17 @@ describe('EC2', () => {
   });
 
   it('Application API endpoint should respond with the correct instance information from EC2 metadata', async () => {
+    const publicInstance = deployedInstances.find((instance) => instance.type === 'public');
+
+    const publicIpv4Address = publicInstance.os.PublicIpAddress;
+    const privateIpv4Address = publicInstance.os.PrivateIpAddress;
+
     const response = await axios.get(`http://${publicIpv4Address}`);
 
-    expect(response.status).to.equal(200);
+    expect(response.status, 'Response status is not correct').to.equal(200);
 
-    expect(response.data.availability_zone).to.equal('us-east-1a');
-    expect(response.data.private_ipv4).to.equal(privateIpv4Address);
-    expect(response.data.region).to.equal('us-east-1');
+    expect(response.data.availability_zone, 'availability_zone is not correct').to.equal('us-east-1a');
+    expect(response.data.private_ipv4, 'private_ipv4 is not correct').to.equal(privateIpv4Address);
+    expect(response.data.region, 'region is not correct').to.equal('us-east-1');
   });
 });
